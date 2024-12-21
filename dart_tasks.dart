f3931ff977e94327b241aac0a477ac3b16e225a8
Ahmed Kachwaa <76598011+ahmedkashwaa@users.noreply.github.com>
2024-12-21 19:45:28 +0200

    categorize list of number task

--- dart_tasks.dart
+++ dart_tasks.dart
@@ -1,30 +1,15 @@
-// This program calculates the total cost of grocery items, including a 10% tax.
-
+// Categorize a list of numbers into even or odd using for loop and switch case
 void main() {
-  // Constants
-  const double taxRate = 0.10;
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  // List of grocery items, each represented as a map with 'name' and 'price'.
-  List<Map<String, dynamic>> groceryItems = [
-    {'name': 'Milk', 'price': 1.5},
-    {'name': 'Bread', 'price': 2.0},
-    {'name': 'Eggs', 'price': 3.0},
-    {'name': 'Cheese', 'price': 4.5},
-  ];
-
-  // Calculate total price of items before tax
-  double totalPriceBeforeTax = 0.0;
-  for (var item in groceryItems) {
-    totalPriceBeforeTax += item['price'];
+  for (var number in numbers) {
+    switch (number % 2) {
+      case 0:
+        print('$number is even');
+        break;
+      case 1:
+        print('$number is odd');
+        break;
+    }
   }
-
-  // Calculate tax and total price after tax
-  double taxAmount = totalPriceBeforeTax * taxRate;
-  double totalPriceAfterTax = totalPriceBeforeTax + taxAmount;
-
-  // Print results
-  print('Total Price Before Tax: \$${totalPriceBeforeTax}');
-  print('Tax Amount (10%): \$${taxAmount}');
-  print('Total Price After Tax: \$${totalPriceAfterTax}');
 }
-
