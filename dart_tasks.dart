f13ff9a977ad64d7489a9dea3c799adfcd5c7daa
Ahmed Kachwaa <76598011+ahmedkashwaa@users.noreply.github.com>
2024-12-25 12:00:19 +0200

    main function

--- dart_tasks.dart
+++ dart_tasks.dart
@@ -1,30 +1,13 @@
-// This program calculates the total cost of grocery items, including a 10% tax.
-
 void main() {
-  // Constants
-  const double taxRate = 0.10;
-
-  // List of grocery items, each represented as a map with 'name' and 'price'.
-  List<Map<String, dynamic>> groceryItems = [
-    {'name': 'Milk', 'price': 1.5},
-    {'name': 'Bread', 'price': 2.0},
-    {'name': 'Eggs', 'price': 3.0},
-    {'name': 'Cheese', 'price': 4.5},
-  ];
-
-  // Calculate total price of items before tax
-  double totalPriceBeforeTax = 0.0;
-  for (var item in groceryItems) {
-    totalPriceBeforeTax += item['price'];
-  }
-
-  // Calculate tax and total price after tax
-  double taxAmount = totalPriceBeforeTax * taxRate;
-  double totalPriceAfterTax = totalPriceBeforeTax + taxAmount;
-
-  // Print results
-  print('Total Price Before Tax: \$${totalPriceBeforeTax}');
-  print('Tax Amount (10%): \$${taxAmount}');
-  print('Total Price After Tax: \$${totalPriceAfterTax}');
+  BankAccount account1 = BankAccount("1234", 1000.0);
+  BankAccount account2 = BankAccount.withZeroBalance("5678");
+  account1.depositMoney(500.0);
+  account1.withdrawalMoney(200.0);
+  account1.withdrawalMoney(1800.0);
+  account1.withdrawalMoney(-50);
+  account1.displayAccountInfo();
+  print("---------------");
+  account2.depositMoney(1000.0);
+  account2.withdrawalMoney(200.0);
+  account2.displayAccountInfo();
 }
-
