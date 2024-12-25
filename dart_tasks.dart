3bbbd3e99278b5ee493257cd7fd485652e751642
Ahmed Kachwaa <76598011+ahmedkashwaa@users.noreply.github.com>
2024-12-25 11:45:01 +0200

    Update dart_tasks.dart

--- dart_tasks.dart
+++ dart_tasks.dart
@@ -1,30 +1,15 @@
-// This program calculates the total cost of grocery items, including a 10% tax.
-
 void main() {
-  // Constants
-  const double taxRate = 0.10;
+  List<int> numbers = [3, 2, 1, 4, 5];
+  var min =minimumNumberOfList(numbers);
+  print("Min number is $min");
 
-  // List of grocery items, each represented as a map with 'name' and 'price'.
-  List<Map<String, dynamic>> groceryItems = [
-    {'name': 'Milk', 'price': 1.5},
-    {'name': 'Bread', 'price': 2.0},
-    {'name': 'Eggs', 'price': 3.0},
-    {'name': 'Cheese', 'price': 4.5},
-  ];
-
-  // Calculate total price of items before tax
-  double totalPriceBeforeTax = 0.0;
-  for (var item in groceryItems) {
-    totalPriceBeforeTax += item['price'];
-  }
-
-  // Calculate tax and total price after tax
-  double taxAmount = totalPriceBeforeTax * taxRate;
-  double totalPriceAfterTax = totalPriceBeforeTax + taxAmount;
-
-  // Print results
-  print('Total Price Before Tax: \$${totalPriceBeforeTax}');
-  print('Tax Amount (10%): \$${taxAmount}');
-  print('Total Price After Tax: \$${totalPriceAfterTax}');
 }
-
+int minimumNumberOfList(List<int> numbers){
+  int min = numbers[0];
+  for (var value in numbers) {
+    if(value<min){
+      min = value;
+    }
+  }
+  return min;
+}
